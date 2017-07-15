093aedeb31b8818ac248e3973a2e539f3bb9b313
Christine Callo <christinejoann.callo@gmail.com>
2017-07-15 14:47:57 -0700

    Displaysbase price of pizza by size without toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
+
+
 function Pizza(size,toppings) {
   this.size=size;
-  this.toppings=toppings;
+  this.toppings=[];
 }
 
-//Pizza.prototype.customerOrder=() {
 
-//}
 
-//var newOrder= new Pizza()
+//Pizza.protype.sizeofPizza = function() {
+  //var type = "";
+  //if ()
+    //return type = 8.99;
+  //} {
+    //return type = 0
+    //console.log("")
+//
+
+
+
+
+
 
 
 
@@ -17,14 +29,36 @@ $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
     $("#review").show();
+    $("#cost").show();
     //console.log("result");
+    var  size = $("input:radio[name=sizing]:checked").val();
+    var  sizePrice ="";
+
+    if (size ==="large") {
+      sizePrice=14.99;
+    } else if (size === "medium") {
+      sizePrice=11.99;
+    } else if (size === "small") {
+      sizePrice=8.99;
+    } else {
+      sizePrice=0;
+    }
+
+
+
+
+
+
+    var size = $("input:radio[name=sizing]:checked").val();
+      $("#sizing").text(size);
     $("input:checkbox[name=veggies]:checked").each(function() {
           var arrayOfVeggies=[];
           var veggies = $(this).val();
           arrayOfVeggies.push(veggies);
         $("#toppings").append(veggies + "<br>");
-
+        $("#price").text(sizePrice);
 
   });
+
 });
 });
