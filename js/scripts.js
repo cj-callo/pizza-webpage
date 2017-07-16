8131ff7bc6ae29b4802f2d907634b0ca7df104c6
Christine Callo <christinejoann.callo@gmail.com>
2017-07-16 07:22:33 -0700

    initial setup of object and prototypes, still need configuring

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,11 @@
-var small=8.99;
-var mediium=11.99;
-var large=14.99;
-var toppings=[];
-
 function Pizza(size,toppings) {
   this.size=size;
-  this.toppings=toppings;
+  this.toppings=[];
 }
 
-
-Pizza.prototype.addToppings=function() {
-  smallToppings = toppings.length + small
+Pizza.prototype.withToppings=function() {
+  var totalCost= (this.toppings.length * .99) + this.size
 }
-
-
-
-
-
 
 
 $(document).ready(function() {
@@ -26,6 +15,7 @@ $(document).ready(function() {
     $("#cost").show();
     //console.log("result");
     var sizePrice = $("input:radio[name=sizing]:checked").val();
+    newOrder = new Pizza (sizePrice, veggies);
     if (sizePrice ==="large") {
       sizePrice=14.99;
     } else if (sizePrice === "medium") {
@@ -36,18 +26,14 @@ $(document).ready(function() {
       sizePrice=0;
     }
     var size = $("input:radio[name=sizing]:checked").val();
-      $("#sizing").text(size);
+    $("#sizing").text(size);
     $("input:checkbox[name=veggies]:checked").each(function() {
-          var arrayOfVeggies=[];
-          var veggies = $(this).val();
-          arrayOfVeggies.push(veggies);
-        $("#toppings").append(veggies + "<br>");
-        $("#price").text(sizePrice);
-
-        var totalCost= $("#price").val();
-
-
+      newOrder.withToppings("");
+      var arrayOfVeggies=[];
+      var veggies = $(this).val();
+      arrayOfVeggies.push(veggies);
+      $("#toppings").append(veggies + "<br>");
+      $("#price").text(sizePrice);
+    });
   });
-
-});
 });
